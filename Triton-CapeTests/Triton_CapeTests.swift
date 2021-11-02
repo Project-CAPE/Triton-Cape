a50ebd2cbb7f7c099bfd184708eb77bfb1ea645d
Tantime <48768802+Tantime@users.noreply.github.com>
2021-11-02 02:44:33 -0700

    Pushing entire Triton CAPE project; also added logout feature

---
+++ Triton-CapeTests/Triton_CapeTests.swift
@@ -0,0 +1,33 @@
+//
+//  Triton_CapeTests.swift
+//  Triton-CapeTests
+//
+//  Created by Khoi Nguyen on 10/26/21.
+//
+
+import XCTest
+@testable import Triton_Cape
+
+class Triton_CapeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
