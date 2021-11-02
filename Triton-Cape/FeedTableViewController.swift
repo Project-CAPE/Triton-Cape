12acdcbb0d81b1a71fcb6d6694704feff102ee5e
Tantime <48768802+Tantime@users.noreply.github.com>
2021-11-02 02:54:07 -0700

    Pushing entire Triton CAPE project; also added logout feature

--- Triton-Cape/FeedTableViewController.swift
+++ Triton-Cape/FeedTableViewController.swift
@@ -80,7 +80,7 @@ class FeedTableViewController: UITableViewController {
     /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    // In a storyboard-based application, you will often want to do a little tpreparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
